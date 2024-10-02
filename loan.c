e3caba9c08c61f752d97f6f2c5d4056119543bd2
Raymond10253 <raymondwafula64@gmail.com>
2024-10-02 13:06:01 -0700

    Add files via upload

---
+++ loan.c
@@ -0,0 +1,27 @@
+//program to calculate simple interest
+/*
+Author: RAYMOND WAFULA
+Reg no.: CT101/G/21897/24
+Date: 23/9/24
+*/
+//preprocessor directive
+#include <stdio.h>
+
+int main(){
+    int age,income;
+
+    printf("Enter your age: ");
+    scanf("%d",&age);
+
+    printf("Enter your annual income: ");
+    scanf("%d", &income);
+
+    if (age >= 21 &&income >=21000) {
+        printf("Congradulations! you qualify for a loan.\n");
+    } else {
+        printf("Unfortunately, we are unable to offer you a loan at this time.\n");
+    }
+
+    return 0;
+
+}
